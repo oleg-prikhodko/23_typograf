da0c77b84b994a0cc1b15a420c46891a957bbe6f
Oleg <super.oleg.3000@yandex.ru>
2018-11-09 21:12:33 +0300

    Add basic api method

--- server.py
+++ server.py
@@ -1,10 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def form():
-    return render_template('form.html')
+    return render_template("form.html")
+
+
+@app.route("/api/process", methods=["POST"])
+def process_by_typographer():
+    # text = request.form["text"]
+    # print("Received:", text)
+    print("Data:", request.get_json().get("text"))
+    return "OK"
+
 
 if __name__ == "__main__":
     app.run()
