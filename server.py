0241a169f52c363ef4b88637193917ccd85bcb87
Oleg <super.oleg.3000@yandex.ru>
2018-11-10 14:36:13 +0300

    Return json response

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, json, abort, Response
 import typographer
 
 app = Flask(__name__)
@@ -11,11 +11,17 @@ def form():
 
 @app.route("/api/process", methods=["POST"])
 def process_by_typographer():
-    # text = request.form["text"]
-    # print("Received:", text)
     text = request.get_json().get("text")
-    # print("Data:", )
-    return typographer.process(text)
+    if not text:
+        abort(
+            Response(
+                status="400",
+                mimetype="application/json",
+                response=json.dumps({"message": "Text is empty"}),
+            )
+        )
+    processed_text = typographer.process(text)
+    return json.jsonify(text=processed_text)
 
 
 if __name__ == "__main__":
